aa924fb55efbf15c162185133183461deab64c18
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2022-04-03 00:36:37 -0300

    Delete Meu_Projeto_WEB-KICK directory

--- Meu_Projeto_WEB-KICK/Js/menu.js
+++
@@ -1,9 +0,0 @@
-function show_menu(){
-    let menu = document.getElementById("my-menu");
-
-    if(menu.className == "menu"){
-        menu.className += " responsive";}
-    else{
-        menu.className = "menu";
-    }
-}
